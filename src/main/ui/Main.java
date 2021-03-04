cdd20cbf0e9d17a7b09496a010cc5e6d2c60e3e3
murchr <murchr@students.cs.ubc.ca>
2021-03-04 00:56:30 -0800

    testing correct merge with GitHub

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,36 +1,7 @@
 package ui;
 
-import java.io.*;
-
-
 public class Main {
     public static void main(String[] args) {
         new RecoveryTraceApp();
     }
 }
-
-
-
-
-
-// potential alternate file storing implementation
-
-/*
-    private static final String fileLocation = "./data/savedAppData";
-    //private static File newFile = new File(fileLocation);
-
-    ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileLocation));
-    //find how to run data
-    RecoveryTraceApp userApplet = ois.readObject();
-    userApplet.runRecoveryTrace();
-
-    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileLocation));
-    oos.writeObject(userApplet);
-    oos.close();
-
-
-    // REQUIRES: n/a
-    // MODIFIES:
-    // EFFECT:
-
- */
